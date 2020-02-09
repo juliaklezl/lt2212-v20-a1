b9365769001ab0f2ee52626434be73d01f0a7ae0
Julia Klezl <julia.klezl@gmail.com>
2020-02-09 22:53:37 +0100

    First version

--- a1.py
+++ a1.py
@@ -3,12 +3,21 @@ import sys
 import pandas as pd
 import numpy as np
 import numpy.random as npr
+from glob import glob
 # ADD ANY OTHER IMPORTS YOU LIKE
 
 # DO NOT CHANGE THE SIGNATURES OF ANY DEFINED FUNCTIONS.
 # YOU CAN ADD "HELPER" FUNCTIONS IF YOU LIKE.
 
 def part1_load(folder1, folder2):
+    dirname_1 = os.path.abspath("../{}".format(folder1)) # not sure if I need this... might be safer to get absolute path, but with ../ I also make it relative...
+    dirname_2 = os.path.abspath("../{}".format(folder2))
+    filenames_1 = glob("{}/*.txt".format(dirname_1))
+    filenames_2 = glob("{}/*.txt".format(dirname_2))
+    for file in filenames_1:
+        print(file)
+        with open (file, "r") as the_file:
+            the_file.read()
     # CHANGE WHATEVER YOU WANT *INSIDE* THIS FUNCTION.
     return pd.DataFrame(npr.randn(2,2)) # DUMMY RETURN
 
@@ -27,3 +36,5 @@ def part3_tfidf(df):
     return df #DUMMY RETURN
 
 # ADD WHATEVER YOU NEED HERE, INCLUDING BONUS CODE.
+
+part1_load("grain", "crude")
