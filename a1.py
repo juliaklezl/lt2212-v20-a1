4d5d6f4ca5ef5bb462de59eda5b010973c1873d3
Julia Klezl <julia.klezl@gmail.com>
2020-02-14 12:02:56 +0100

    part 1 and 2 done

--- a1.py
+++ a1.py
@@ -9,35 +9,42 @@ from glob import glob
 # DO NOT CHANGE THE SIGNATURES OF ANY DEFINED FUNCTIONS.
 # YOU CAN ADD "HELPER" FUNCTIONS IF YOU LIKE.
 
-def part1_load(folder1, folder2, n=2):  # I guess I can just specify that folder1 and folder2 have to be relative pathnames
+def part1_load(folder1, folder2, n=2):  # folder1 and folder2 need to be full paths (if folders not in same directory)
     # CHANGE WHATEVER YOU WANT *INSIDE* THIS FUNCTION.
-    dirname_1 = os.path.abspath("../{}".format(
-        folder1))  # TODO: not sure if I need this... might be safer to get absolute path, but with ../ I also make it relative...
-    dirname_2 = os.path.abspath("../{}".format(folder2))
-    filenames_1 = glob("{}/*.txt".format(dirname_1))
-    filenames_2 = glob("{}/*.txt".format(dirname_2))
-    #alphabet = []
-    data = []
-    for file in filenames_1:  # there has to be a better way to get the alphabet...
+    list1 = files_to_dictlist(folder1)
+    list2 = files_to_dictlist(folder2)
+    data = list1 + list2
+    df = pd.DataFrame(data)
+    df = df.fillna(0)
+    dx = df.sum()
+    droplist = [i for i in list(dx.index[2:]) if dx[i] < n]
+    df_dropped = df.drop(droplist, 1)
+    return df_dropped
+
+def files_to_dictlist(directory):  # function to get files into list of dicts for dataframe, in order to avoid duplicate code for the 2 directories
+    directory_data = []
+    filenames = glob("{}/*.txt".format(directory))
+    for file in filenames:
         df_line = {}
-        df_line['doc_name'] = file.split("/")[-1]  # TODO: name is actually path here...
-        df_line['class'] = folder1  # TODO: This whole thing probably needs to be a helper function.. be careful with class label then
+        df_line['doc_name'] = file # keeping whole path as name since some files have same name across directories
+        df_line['class_name'] = directory.split("/")[-1] # class name should not be full path, only folder name
         with open(file, "r") as the_file:
             wholetext = the_file.readlines()
             words = []
             for line in wholetext:
-                words.extend(line.split())  # TODO: This gives me a lot of numbers too... and punctuation...
+                for word in line.split():
+                    if word.isalpha(): # get rid of punctuation and numbers and lowercase all words
+                        words.append(word.lower())
             for word in words:
-                #if word not in alphabet:
-                 #   alphabet.append(word)
-                if word in df_line:  # TODO: think about whether you should tokenize, lowercase,... etc here...
+                if word in df_line:  # TODO: tokenize???
                     df_line[word] += 1
                 else:
                     df_line[word] = 1
-        data.append(df_line)
+        directory_data.append(df_line)
+    return(directory_data)
     df = pd.DataFrame(data)
     df = df.fillna(0)
-    dx = df.sum()  # TODO: check if there is an easier way to do this - collection counter
+    dx = df.sum()
     droplist = [i for i in list(dx.index[2:]) if dx[i] < n]
     df_dropped = df.drop(droplist, 1)
     return df_dropped
@@ -46,14 +53,12 @@ def part2_vis(df, m):
     # DO NOT CHANGE
     assert isinstance(df, pd.DataFrame)
     # CHANGE WHAT YOU WANT HERE
-
-
     df_sum = df.sum()[2:].sort_values(ascending = False)
     df_lower = df_sum[m:]
-    df_top = df.drop(df_lower.index, 1).groupby("class")
-    print(df_top)
-    #df_small = df.iloc[:10, :10]
-    print(df_top.plot(kind="bar"))
+    df_top = df.drop(df_lower.index, 1) # get rid of all columns that are not in top m
+    df_top_grouped = df_top.groupby(['class_name']).sum()
+    print(df_top_grouped)
+    print(df_top_grouped.T.plot(kind="bar"))
 
     #df.groupby('year').case_status.value_counts().unstack(0).plot.barh()
 
